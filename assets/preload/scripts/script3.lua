abed75a8d76fe98e34d5ce047c418bdfeac30097
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2022-12-27 16:24:24 -0500

    Add files via upload

---
+++ assets/preload/scripts/script3.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark',"Port by OswaldGamerYT", 0, 0, 0); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 22);
+    addLuaText('watermark');
+end
